3000f1125567e153200f527feaeb4cf42b16a0da
in0cc1.inoue <k.prfm19@gmail.com>
2017-06-11 13:04:03 +0900

    storyboardにviewを配置、cellにviewを配置、cellのオートレイアウト、vc.swiftを準備

---
+++ ShareArticle/ArticleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.swift
+//  ShareArticle
+//
+//  Created by Wataru Inoue on 2017/06/11.
+//  Copyright © 2017年 Wataru Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
