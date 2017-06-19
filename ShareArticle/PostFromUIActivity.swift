6ba15136fe0cf4a0f78ecd392e89e0d158f03142
井上航 <k.prfm19@gmail.com>
2017-06-19 16:15:10 +0900

    uiactivityのためのclassファイルを新しく作った。昔のプロジェクトを改良中

---
+++ ShareArticle/PostFromUIActivity.swift
@@ -0,0 +1,13 @@
+//
+//  PostFromUIActivity.swift
+//  ShareArticle
+//
+//  Created by Wataru Inoue on 2017/06/19.
+//  Copyright © 2017年 Wataru Inoue. All rights reserved.
+//
+
+import UIKit
+
+class PostFromUIActivity: UIActivity {
+
+}
