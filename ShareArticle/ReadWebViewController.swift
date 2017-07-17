7178cbd2df5d55f4a4173d93f48e77f24af02c55
in0cc1.inoue <k.prfm19@gmail.com>
2017-07-18 00:09:15 +0900

    webのicon追加した

--- ShareArticle/ReadWebViewController.swift
+++ ShareArticle/ReadWebViewController.swift
@@ -11,15 +11,16 @@ import WebKit
 
 class ReadWebViewController: UIViewController, WKNavigationDelegate{
     
-//    @IBOutlet weak var webView: UIWebView!
+    //    @IBOutlet weak var webView: UIWebView!
     let webView = WKWebView()
     let progressView = UIProgressView()
+    let toolbar = UIToolbar()
     
-    @IBOutlet weak var backButton: UIBarButtonItem!
-    @IBOutlet weak var nextButton: UIBarButtonItem!
-    @IBOutlet weak var stopButton: UIBarButtonItem!
-    @IBOutlet weak var loadButton: UIBarButtonItem!
-    @IBOutlet weak var actionButton: UIBarButtonItem!
+    var backButtonItem: UIBarButtonItem!
+    var nextButtonItem: UIBarButtonItem!
+    var stopButtonItem: UIBarButtonItem!
+    var loadButtonItem: UIBarButtonItem!
+    var actionButtonItem: UIBarButtonItem!
     
     var originUrl: URL! // 前のvcから引き継いでくる
     var currentURL: URL!
@@ -43,6 +44,18 @@ class ReadWebViewController: UIViewController, WKNavigationDelegate{
         self.navigationController?.navigationBar.addSubview(progressView)
         
         webView.load(URLRequest(url: originUrl))
+        
+        toolbar.frame = CGRect(x: 0, y: webView.bottomY, width: self.view.frame.width, height: ViewSize.toolbarHeight)
+        backButtonItem = UIBarButtonItem(image: IconImage.backImage(isOn: false), style: .plain, target: nil, action: #selector(ReadWebViewController.onTappedBackButton(_:)))
+        nextButtonItem = UIBarButtonItem(image: IconImage.nextImage(isOn: false), style: .plain, target: nil, action: #selector(ReadWebViewController.onTappedNextButton(_:)))
+        stopButtonItem = UIBarButtonItem(image: IconImage.stopImage(isOn: false), style: .plain, target: nil, action: #selector(ReadWebViewController.onTappedStopButton(_:)))
+        loadButtonItem = UIBarButtonItem(image: IconImage.loadImage(isOn: true), style: .plain, target: nil, action: #selector(ReadWebViewController.onTappedLoadButton(_:)))
+        let flexibleItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let spaceItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spaceItem.width = 20
+        actionButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(ReadWebViewController.onTappedActionButton(_:)))
+        toolbar.items = [backButtonItem, spaceItem, nextButtonItem, spaceItem, stopButtonItem, spaceItem, loadButtonItem, flexibleItem, actionButtonItem]
+        self.view.addSubview(toolbar)
         
         setAllControlButtonsStatus()
     }
@@ -79,24 +92,45 @@ class ReadWebViewController: UIViewController, WKNavigationDelegate{
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onTappedBackButton(_ sender: UIBarButtonItem) {
+    //    @IBAction func onTappedBackButton(_ sender: UIBarButtonItem) {
+    //        guard webView.canGoBack else { return }
+    //        webView.goBack() // 戻る
+    //    }
+    //
+    //    @IBAction func onTappedNextButton(_ sender: UIBarButtonItem) {
+    //        guard webView.canGoForward else { return }
+    //        webView.goForward() // 進む
+    //    }
+    //    @IBAction func onTappedStopButton(_ sender: UIBarButtonItem) {
+    //        webView.stopLoading() // 読み込み停止
+    //    }
+    //
+    //    @IBAction func onTappedLoadButton(_ sender: UIBarButtonItem) {
+    //        webView.reload() // 再度読み込み
+    //    }
+    //
+    //    @IBAction func onTappedActionButton(_ sender: UIBarButtonItem) {
+    //        showUiActivity()
+    //    }
+    //
+    func onTappedBackButton(_ sender: UIBarButtonItem) {
         guard webView.canGoBack else { return }
         webView.goBack() // 戻る
     }
     
-    @IBAction func onTappedNextButton(_ sender: UIBarButtonItem) {
+    func onTappedNextButton(_ sender: UIBarButtonItem) {
         guard webView.canGoForward else { return }
         webView.goForward() // 進む
     }
-    @IBAction func onTappedStopButton(_ sender: UIBarButtonItem) {
+    func onTappedStopButton(_ sender: UIBarButtonItem) {
         webView.stopLoading() // 読み込み停止
     }
     
-    @IBAction func onTappedLoadButton(_ sender: UIBarButtonItem) {
+    func onTappedLoadButton(_ sender: UIBarButtonItem) {
         webView.reload() // 再度読み込み
     }
     
-    @IBAction func onTappedActionButton(_ sender: UIBarButtonItem) {
+    func onTappedActionButton(_ sender: UIBarButtonItem) {
         showUiActivity()
     }
     
@@ -129,24 +163,47 @@ class ReadWebViewController: UIViewController, WKNavigationDelegate{
         // vcのタイトルにホスト名を表示する
         guard let url = url else { return }
         if let component = URLComponents(string: url.absoluteString) {
-//            self.navigationController?.navigationBar.topItem!.title = component.host
+            //            self.navigationController?.navigationBar.topItem!.title = component.host
             self.title = component.host //
         }
     }
 }
 
-private extension ReadWebViewController {
+extension ReadWebViewController {
     func setAllControlButtonsStatus() {
         // webviewの状態に応じて、3つ全てのボタンの色と操作許可を変更
-        backButton.setState(isAvailable: webView.canGoBack)
-        nextButton.setState(isAvailable: webView.canGoForward)
-        stopButton.setState(isAvailable: webView.isLoading)
+        guard let back = backButtonItem,
+            let next = nextButtonItem,
+            let stop = stopButtonItem else { return }
+        back.setState(isAvailable: webView.canGoBack)
+        back.image = IconImage.backImage(isOn: webView.canGoBack)
+        next.setState(isAvailable: webView.canGoForward)
+        next.image = IconImage.nextImage(isOn: webView.canGoForward)
+        stop.setState(isAvailable: webView.isLoading)
+        stop.image = IconImage.stopImage(isOn: webView.isLoading)
     }
 }
 
 private extension UIBarButtonItem {
     func setState(isAvailable: Bool) {
         self.isEnabled = isAvailable
-        self.tintColor = isAvailable ? UIColor.blue : UIColor.gray
+        //        self.tintColor = isAvailable ? UIColor.blue : UIColor.gray
+    }
+}
+
+struct IconImage {
+    static func backImage(isOn: Bool) -> UIImage {
+        return isOn ? UIImage(named: "back_on.png")! : UIImage(named: "back_off.png")!
+    }
+    
+    static func nextImage(isOn: Bool) -> UIImage {
+        return isOn ? UIImage(named: "next_on.png")! : UIImage(named: "next_off.png")!
+    }
+    
+    static func stopImage(isOn: Bool) -> UIImage {
+        return isOn ? UIImage(named: "stop_on.png")! : UIImage(named: "stop_off.png")!
+    }
+    static func loadImage(isOn: Bool) -> UIImage {
+        return isOn ? UIImage(named: "reload_on.png")! : UIImage(named: "reload_off.png")!
     }
 }
